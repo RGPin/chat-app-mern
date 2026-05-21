812229f73538c5df1ded720b4da7589fe70bb0b2
RGPin <157803400+RGPin@users.noreply.github.com>
2026-05-21 14:35:15 +0800

    Backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, (error) => {
+  if (error) throw error;
+  console.log(`Server is running on port ${PORT}`);
+});
