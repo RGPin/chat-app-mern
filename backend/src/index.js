c5af3da0965e2a8aeb3ef276d962f2938c7382d9
RGPin <157803400+RGPin@users.noreply.github.com>
2026-06-03 15:02:01 +0800

    messages router

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, (error) => {
